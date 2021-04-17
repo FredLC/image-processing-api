6523b816db982d16f248140a74feb74cf122def5
fredlc <fredlefevre@outlook.com>
2021-04-17 12:45:22 -0400

    Implemented image processing functionality using the sharp library

--- src/routes/api/images.ts
+++ src/routes/api/images.ts
@@ -1,9 +1,37 @@
 import express from 'express';
+import { promises as fsPromises } from 'fs';
+const sharp = require('sharp');
 
 const images = express.Router();
 
-images.get('/', (req, res) => {
-  res.send('Hello from images!');
+images.get('/', async (req, res) => {
+  const filename = req.query.filename;
+  const width = parseInt(req.query.width as string);
+  const height = parseInt(req.query.height as string);
+  const imagePath = `./src/images/${filename}.jpg`;
+  const jpegImage = await sharp(imagePath).jpeg({ mozjpeg: true }).toBuffer();
+  const resizedImage = await sharp(jpegImage)
+    .resize(width, height)
+    .toFormat('jpeg')
+    .toBuffer();
+  await fsPromises.writeFile(
+    `./src/images/${filename}-thumb.jpeg`,
+    resizedImage
+  );
+  try {
+    const thumbImage = await fsPromises.readFile(
+      `./src/images/${filename}-thumb.jpeg`,
+      'base64'
+    );
+    const imageUrl = `data:image/jpeg;base64,${thumbImage}`;
+    res.send(`<img src=${imageUrl}>`);
+  } catch (err) {
+    if (err) {
+      res.send(
+        'There was an error processing your image. Please double check the name of your file.'
+      );
+    }
+  }
 });
 
 export default images;
